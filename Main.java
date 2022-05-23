8d496738a626989b7f6092b56a04f0115c4ce27c
Nayjk <98636064+LobkovaLiza@users.noreply.github.com>
2022-05-23 14:29:46 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,13 +3,20 @@ public class Main {
     public static void main(String[] args) {
         String str = "Java jav javas";
         String[] words = str.split(" ");
-        for (String word:words){
-            System.out.println(word);
-        }
+        System.out.println("Самое длинное слово: " + minLength(words));
         if (str.isEmpty()){
             System.out.println("Пустая строка");
         }
-
-       // for (int i = 0; i < words.length; i++){ }
+    }
+    private static String minLength(String[] s) {
+        int len = 0;
+        String shortest = "";
+        for (String string : s) {
+            if (string.length() < len) {
+                len = string.length();
+                shortest = string;
+            }
+        }
+        return shortest;
     }
 }
