32a2881e15eea7fe2b7d565ed7d993ca251bfc58
Nayjk <98636064+LobkovaLiza@users.noreply.github.com>
2022-05-23 14:53:59 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
 
-class ShortWords{
-    public ShortWords(String a) {
-        String result;
-        String[] words = a.split(" ");
-        int max = 100000;
-        for (String word : words) {
+class Short{
+    public Short(String c) {
+        String[] words = c.split(" ");
+        int max = 99;
+        for (String word:words) {
             int min = word.length();
             if (min< max){
                 max = min;
@@ -13,19 +12,17 @@ class ShortWords{
         }
         for (String array : words){
             if(array.length() == max){
-                result = array;
-                System.out.println(array);
+                System.out.println("Самое короткое слово буит: " + array);
                 break;
             }
         }
     }
 }
 public class Main {
-
     public static void main(String[] args) {
-	Scanner in = new Scanner(System.in);
-    String a = in.nextLine();
-    ShortWords ds = new ShortWords(a);
-
+        Scanner in = new Scanner(System.in);
+        System.out.println("Ведите строчку пжалста");
+        String a = in.nextLine();
+        Short ds = new Short(a);
     }
 }
